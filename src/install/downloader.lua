f6f487b1261733c8f98fb64ee1a057d072b3e842
Reycko <78082869+Reycko@users.noreply.github.com>
2024-12-23 22:37:14 +0100

    feat: rework downloader and installer to be robust and automatic

--- src/install/downloader.lua
+++ src/install/downloader.lua
@@ -1,8 +1,133 @@
+---@diagnostic disable: deprecated
 --- Advanced Reylib downloader
 --- @return boolean succeded
 
 -- If you wanna paste from this, note that the "modules" are just necessary functions to prevent bloat thus are incomplete
 
+--[[===Library definitions===]]--
+-- These are defined here because this downloader should have no dependencies.
+
+--[[
+We'll use versionfns instead of the `Version` class as it's more portable and this script is meant to stay short.
+]]
+
+---Multiple helper functions for versions
+local versionfns = {
+  ---Parses the version \
+  ---Unlike the `Version` class, this returns it as a table instead of a tuple
+  ---to fit the other functions
+  ---@deprecated Use version class instead
+  ---@param version string
+  ---@return table versionTable
+  parse = function (version)
+    local found = {} ---@type number[]
+
+    for v in version:gmatch("[^%.]+") do -- gmatch returns an iterator, so we can't directly put it in found
+      local number = tonumber(v, 10)
+      if (not number or number < 0) then error("Invalid version '" .. version .. "'", 2) end
+
+      table.insert(found, number)
+    end
+
+    if (#found ~= 3) then error("invalid semantic version (expected 3 values, got " .. #found .. ")") end
+    return found
+  end,
+
+  ---Compares two versions and returns whether or not the current version is the same
+  ---@deprecated Use version class instead
+  ---@param current number[] Current version, as a table
+  ---@param other number[] Other version, as a table
+  ---@return boolean
+  equal = function (current, other)
+    if (#other ~= 3) then error("bad argument #2 (expected 3 values, got " .. #other .. ")", 2) end
+    if (#current ~= 3) then error("bad argument #1 (expected 3 values, got " .. #other .. ")", 2) end
+
+    for i in #current do
+      if (current[i] ~= other[i]) then return false end
+    end
+
+    return true
+  end,
+
+  ---Compares two versions and returns whether or not the current version is lower
+  ---@deprecated Use version class instead
+  ---@param current number[] Current version, as a table
+  ---@param other number[] Other version, as a table
+  ---@return boolean
+  lessThan = function (current, other)
+    if (#other ~= 3) then error("bad argument #2 (expected 3 values, got " .. #other .. ")", 2) end
+    if (#current ~= 3) then error("bad argument #1 (expected 3 values, got " .. #other .. ")", 2) end
+
+    for i=1,#current do
+      if (current[i] < other[i]) then
+        return true
+      elseif (current[i] > other[i]) then
+        return false
+      end
+    end
+
+    return false
+  end,
+
+  ---Compares two versions are returns whether or not the current version is greater
+  ---@deprecated Use version class instead
+  ---@param current any
+  ---@param other any
+  ---@return boolean
+  greaterThan = function (current, other)
+    if (#other ~= 3) then error("bad argument #2 (expected 3 values, got " .. #other .. ")", 2) end
+    if (#current ~= 3) then error("bad argument #1 (expected 3 values, got " .. #other .. ")", 2) end
+
+    for i=1,#current do
+      if (current[i] > other[i]) then
+        return true
+      elseif (current[i] < other[i]) then
+        return false
+      end
+    end
+
+    return false
+  end,
+}
+
+local github
+github = {
+  ---Gets a file from a GitHub repo
+  ---@param repo string Repository to look in, should be formatted as Author/Repo
+  ---@param location string String location of the file
+  ---@param branch? string Defaults to 'master'
+  ---@return string File data as string
+  getFile = function (repo, location, branch)
+    branch = branch or "master"
+    local response = http.get("https://raw.githubusercontent.com/" .. textutils.urlEncode(repo) .. "/refs/heads/" .. textutils.urlEncode(branch) .. "/" .. location .. "?cb=" .. ("%x"):format(math.random(0, 2^30)))
+    if (response) then
+      local res = response.readAll()
+      response.close()
+      return res
+    end
+
+    return ""
+  end,
+
+  ---Gets a file from a GitHub repo, then executes it
+  ---@param repo string Repository to look in, should be formatted as Author/Repo
+  ---@param location string String location of the file
+  ---@param branch? string Defaults to 'master'
+  ---@param env? table Environment to pass, defaults to _ENV
+  ---@param ... any Arguments to pass to the script
+  ---@return any? return Whatever the executed script returns, or nil if it couldn't be executed
+  runFile = function (repo, location, branch, env, ...)
+    branch = branch or "master"
+    env = env or _ENV
+    local file = github.getFile(repo, location)
+
+    load(file, location .. "@" .. repo .. " (GitHub)", "t", env)(...)
+  end
+}
+
+--[[===Code===]]--
+
+-- Parse arguments
 local args = { ---@type { [string]: boolean }
   ["auto"] = false,
   ["silent"] = false,
@@ -17,58 +142,11 @@ for _, rawArg in pairs({ ... }) do
   local setTo = true
   if (arg:sub(1, 3) == "no_" and #arg > 3) then
     setTo = false
-    arg = arg:sub(4, #arg)
+    arg = arg:sub(4)
   end
 
   args[arg:lower()] = setTo
 end
-
-local versions = {
-  ---Parses the version
-  ---@param version string
-  ---@return table versionTable
-  parse = function (self, version)
-    local versionTable = {}
-
-    for v in version:gmatch("[^%.]+") do
-      table.insert(versionTable, tonumber(v))
-    end
-
-    return versionTable
-  end,
-
-  ---Compares two versions and returns whether or not the version is the same
-  ---@param current number[] Current version, as a table
-  ---@param other number[] Other version, as a table
-  ---@return boolean
-  equal = function (self, current, other)
-    if (#current ~= #other) then return false end
-
-    for i in #current do
-      if (current[i] ~= other[i]) then return false end
-    end
-
-    return true
-  end,
-
-  ---Compares two versions and returns whether or not the version is lower
-  ---@param current number[] Current version, as a table
-  ---@param other number[] Other version, as a table
-  ---@return boolean
-  lessThan = function (self, current, other)
-    if (#current ~= #other) then return false end
-
-    for i=1,#current do
-      if (current[i] < other[i]) then
-        return true
-      elseif (current[i] > other[i]) then
-        return false
-      end
-    end
-
-    return false
-  end
-}
 
 ---Finds Reylib
 ---@return boolean found, table version
@@ -84,7 +162,7 @@ local function findReylib()
     if (f ~= nil) then
       found = true
 ---@diagnostic disable-next-line: undefined-field
-      version = versions:parse(f:read("*l"))
+      version = versionfns.parse(f:read())
 
       f:close()
     end
@@ -102,8 +180,8 @@ local function ask(question, choices, default_no)
   default_no = default_no or false
   choices = choices or {"y", "n"}
 
-  local autocomplete_function = function (partial) require("cc.completion").choice(partial, {"y", "n"}) end
-  print(question .. "[" .. (not default_no and choices[1]:upper() or choices[1]:lower()) .. "/" .. (default_no and choices[2]:upper() or choices[2]:lower()) .. "]")
+  local autocomplete_function = function (partial) require("cc.completion").choice(partial, choices) end
+  print(question .. " [" .. (not default_no and choices[1]:upper() or choices[1]:lower()) .. "/" .. (default_no and choices[2]:upper() or choices[2]:lower()) .. "]")
   local input = read(nil, nil, autocomplete_function):lower():sub(1, 1) --- @type string
   if (default_no) then
     return input == choices[1]:lower()
@@ -118,48 +196,16 @@ local function print(...)
   if (not args["silent"]) then _print(...) end
 end
 
----@class Github
-local Github = {
-  ---Gets a file from a GitHub repo
-  ---@param self Github
-  ---@param repo string Repository to look in, should be formatted as Author/Repo
-  ---@param location string String location of the file
-  ---@param branch? string Defaults to 'master'
-  ---@return string File data as string
-  getFile = function (self, repo, location, branch)
-    branch = branch or "master"
-    local response = http.get("https://raw.githubusercontent.com/" .. textutils.urlEncode(repo) .. "/refs/heads/" .. textutils.urlEncode(branch) .. "/" .. location .. "?cb=" .. ("%x"):format(math.random(0, 2^30)))
-    if (response) then
-      local res = response.readAll()
-      response.close()
-      return res
-    end
-
-    return ""
-  end,
-
-  ---Gets a file from a GitHub repo, then executes it
-  ---@param self Github
-  ---@param repo string Repository to look in, should be formatted as Author/Repo
-  ---@param location string String location of the file
-  ---@param branch? string Defaults to 'master'
-  ---@param env? table Environment to pass, defaults to _ENV
-  ---@param ... any Arguments to pass to the script
-  ---@return any? return Whatever the executed script returns, or nil if it couldn't be executed
-  runFile = function (self, repo, location, branch, env, ...)
-    branch = branch or "master"
-    env = env or _ENV
-    local file = self:getFile(repo, location)
-
-    load(file, location .. "@" .. repo .. " (GitHub)", "t", env)(...)
-  end
-}
-
 ---Main function.
 ---@return boolean succeded
 local function main()
   if (not http) then
-    print("HTTP has to be enabled in the server config. If you are admin, set http.enabled to true in the server's config.")
+    print("HTTP has to be enabled in the server config.\nIf you are admin, set http.enabled to true in the server's config.")
+    return false
+  end
+
+  if (not http.checkURL("https://github.com")) then
+    print("Couldn't contact GitHub.\nIf you are admin, make sure github.com is allowed in the server's config.\nIn singleplayer, make sure you are connected to the internet.")
     return false
   end
 
@@ -169,20 +215,23 @@ local function main()
     if (not args["auto"] and ask("Reylib is already downloaded (v" .. table.concat(rlVersion, ".") .. ").\nDo you want to update [u] or remove [r]?", {"u", "r"}) or (args["update"] and not args["remove"])) then
       -- update
       local shouldUpdate = true
-      local rawLatestVersion = Github:getFile("Reycko/CCT-Reylib", "VERSION")
+      local rawLatestVersion = github.getFile("Reycko/CCT-Reylib", "VERSION")
       if (not rawLatestVersion) then
         print("WARN: Couldn't fetch what the lastest version is")
       else
-        shouldUpdate = versions:lessThan(rlVersion, versions:parse(rawLatestVersion))
+        shouldUpdate = versionfns.lessThan(rlVersion, versionfns.parse(rawLatestVersion))
+        if (not args["auto"] and not shouldUpdate) then
+          shouldUpdate = ask("You already have the latest version of Reylib.\nAre you sure you want to download it anyway?", nil, true)
+        end
 
         print("Downloading " .. rawLatestVersion .. ".")
-        Github:runFile("Reycko/CCT-Reylib", "src/install/install.lua", "master", _G)
+        github.runFile("Reycko/CCT-Reylib", "src/install/install.lua", "master", _G)
       end
     else
       -- remove
-      if (not fs.exists("/libs/reylib/remove.lua")) then
+      if (not fs.exists("/libs/reylib/programs/remove.lua")) then
         print("Couldn't find remover. Downloading latest one.")
-        Github:runFile("Reycko/CCT-Reylib", "src/install/remove.lua", "master", _G)
+        github.runFile("Reycko/CCT-Reylib", "src/install/remove.lua", "master", _G)
       else
 ---@diagnostic disable-next-line: undefined-field
         os.run({}, "/libs/reylib/remove.lua")
@@ -192,7 +241,7 @@ local function main()
   else
     if (not args["auto"] and ask("Do you want to download Reylib?") or (args["download"])) then
           print("Downloading Reylib.")
-          Github:runFile("Reycko/CCT-Reylib", "src/install/install.lua", "master", _G)
+          github.runFile("Reycko/CCT-Reylib", "src/install/install.lua", "master", _G)
 
           return true
     end
--- src/install/install.lua
+++ src/install/install.lua
@@ -1,20 +1,34 @@
+
+--[[Seperated version of the GitHub API to make this independent]]--
+local function checkHttp()
+  if (not http) then
+    error("no http library", 3)
+  end
+
+  if (not http.checkURL("https://github.com")) then
+    error("can't contact github", 3)
+  end
+end
+
 local github
 github = {
   ---Gets a file from a GitHub repo
   ---@param repo string Repository to look in, should be formatted as Author/Repo
   ---@param location string String location of the file
   ---@param branch? string Defaults to 'master'
-  ---@return string File data as string
+  ---@return string file data as string
   getFile = function (repo, location, branch)
+    checkHttp()
     branch = branch or "master"
-    local response = http.get("https://raw.githubusercontent.com/" .. textutils.urlEncode(repo) .. "/refs/heads/" .. textutils.urlEncode(branch) .. "/" .. location .. "?cb=" .. ("%x"):format(math.random(0, 2^30)))
+    local response, err = http.get("https://raw.githubusercontent.com/" .. textutils.urlEncode(repo) .. "/refs/heads/" .. textutils.urlEncode(branch) .. "/" .. location .. "?cb=" .. ("%x"):format(math.random(0, 2^30)))
     if (response) then
       local res = response.readAll()
       response.close()
+      if (not res) then error("couldn't read file (empty response, possibly nonexistent file)") end
       return res
     end
 
-    return ""
+    error("couldn't read file (" .. tostring(err) .. ")", 2)
   end,
 
   ---Gets a file from a GitHub repo, then executes it
@@ -25,41 +39,154 @@ github = {
   ---@param ... any Arguments to pass to the script
   ---@return any? return Whatever the executed script returns, or nil if it couldn't be executed
   runFile = function (repo, location, branch, env, ...)
+    checkHttp()
     branch = branch or "master"
-    env = env or _ENV
     local file = github.getFile(repo, location)
 
-    load(file, location .. "@" .. repo .. " (GitHub)", "t", env)(...)
-  end
+    local func, err = load(file, location .. "@" .. repo .. " (GitHub)", "t", env)
+    if (not func) then error("couldn't load file (" .. err .. ")", 2) end
+    pcall(func, ...)
+  end,
+
+  ---Gets the contents of a folder from a GitHub repo. \
+  ---This API is limited to 1000 files. \
+  ---[GitHub REST API Documentation](https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content)
+  ---@param repo string Repository to look in, should be formatted as Author/Repo
+  ---@param location? string String location of the folder, leave empty for repository root
+  ---@param branch? string Defaults to 'master'
+  ---@return GitHubFolder data Data of the folder.
+  getFolder = function (repo, location, branch)
+    location = location or ""
+    --TODO: handle symlinks and submodules (https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content#:~:text=If%20the%20content%20is%20a%20symlink,will%20have%20null%20values.)
+    checkHttp()
+    local query_params = ""
+    if (branch) then query_params = "?ref=" .. branch end
+    local response, err = http.get("https://api.github.com/repos/" .. repo .. "/contents/" .. location .. query_params, {
+      ["Content-Type"] = "application/json",
+      ["User-Agent"] = "Reylib",
+    })
+
+    if (response) then
+      local raw_res = response.readAll()
+      response.close()
+      if (not raw_res) then error("couldn't read response (", 1) end
+
+      local res, unserialize_err = textutils.unserialiseJSON(raw_res, {
+        parse_null = true,
+        parse_empty_array = true,
+        nbt_style = false,
+      })
+
+      res = res --[[@as GitHubFolder]]
+
+      return res and res or error(unserialize_err, 2)
+    else
+      error("bad argument #1|#2 (" .. err .. ")")
+    end
+  end,
 }
 
-local FILES = {
-  ["src/paste.lua"] = "paste.lua",
-  ["src/github.lua"] = "github.lua",
-  ["src/versionfns.lua"] = "versionfns.lua",
-  ["src/version.lua"] = "version.lua",
-  ["VERSION"] = "VERSION",
+--[[Code]]--
+
+---List of Lua patterns that, if matched on the full path, will not be downloaded
+local excludePatterns = {
+  "src/install/.*"
 }
 
-local RUN = {
+---@type table<string, table> Full path, args to pass
+local installScripts = {
   ["src/install/mkstartup.lua"] = {},
 }
 
-fs.delete("/libs/reylib")
-fs.makeDir("/libs/reylib")
-for location, file in pairs(FILES) do
-  local data = github.getFile("Reycko/CCT-Reylib", location)
-  if (not data) then
-    print("WARN: Couldn't get file " .. location .. " (supposed to go in " .. file .. ")")
+-- Parse arguments
+local args = { ---@type { [string]: boolean }
+  ["silent"] = false,
+}
+
+for _, rawArg in pairs({ ... }) do
+  rawArg = type(rawArg) == "string" and rawArg or "" ---@type string
+  local arg = rawArg
+  local setTo = true
+  if (arg:sub(1, 3) == "no_" and #arg > 3) then
+    setTo = false
+    arg = arg:sub(4)
   end
 
-  local f = io.open("/libs/reylib/" .. file, "w")
-  if (not f) then goto continue end
-
-  f:write(data or "--Couldn't fetch!")
-  ::continue::
+  args[arg:lower()] = setTo
 end
 
-for location, args in pairs(RUN) do
-  github.runFile("Reycko/CCT-Reylib", location, "master", _G, table.unpack(args))
+fs.delete("/libs/reylib")
+fs.makeDir("/libs/reylib")
+
+---Downloads a file
+---@param path string Path
+---@return boolean downloaded
+local function downloadFile(path)
+  local data = github.getFile("Reycko/CCT-Reylib", path)
+
+  local f, err = fs.open("/libs/reylib/" .. (path:sub(1, 4) == "src/" and path:sub(4) or path), "w")
+  if (not f) then error("couldn't open file (" .. err .. ")", 2) end
+
+  f.write(data)
+
+  return true
 end
+
+
+local loadFiles
+
+---@param path string
+---@return table
+loadFiles = function (path)
+  local found = {}
+  for _, file_data in pairs(github.getFolder("Reycko/CCT-Reylib", path)) do
+    if (file_data.type == "dir") then
+      for _, file in pairs(loadFiles(file_data.path)) do
+        table.insert(found, file)
+      end
+    elseif (file_data.type == "file") then
+      local pattern_matched = false
+      for _, pattern in pairs(excludePatterns) do
+        if (file_data.path:match(pattern)) then
+          pattern_matched = true
+          break
+        end
+      end
+
+      if (not pattern_matched) then
+        table.insert(found, file_data.path)
+      end
+    end
+  end
+
+  return found
+end
+
+local files = loadFiles("src")
+table.insert(files, "VERSION")
+
+local _print = print
+---Wraps print to hide when silent.
+local function print(...)
+  if (not args["silent"]) then _print(...) end
+end
+
+print("[Reylib installer started]")
+
+for current_file, file in pairs(files) do
+  downloadFile(file)
+  print("Downloaded " .. file .. " (" .. current_file .. "/" .. #files .. ")")
+end
+
+print("[Running install scripts]")
+local scripts_ran = 0
+local scripts_to_run = 0
+for _, _ in pairs(installScripts) do scripts_to_run = scripts_to_run + 1 end -- HACK: #tbl only works for 'array' tables
+-- Run install scripts
+for location, args in pairs(installScripts) do
+  github.runFile("Reycko/CCT-Reylib", location, "master", nil, table.unpack(args))
+  scripts_ran = scripts_ran + 1
+  print("Ran install script " .. location .. " (" .. scripts_ran .. "/" .. scripts_to_run .. ")")
+end
+
+print("[Reylib installed successfully]")
