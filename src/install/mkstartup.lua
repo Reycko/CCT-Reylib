8d42f53867b4aaa5861ac58f6543e95615aac397
Reycko <78082869+Reycko@users.noreply.github.com>
2024-12-23 23:04:49 +0100

    refactor(install): remove useless variable declarations in mkstartup

--- src/install/mkstartup.lua
+++ src/install/mkstartup.lua
@@ -1,14 +1,11 @@
-local Github = require(".libs.reylib.github") ---@module 'src.github'
-
-local data = [[---Reylib auto updater, avoid touching this file.
-
-require(".libs.reylib.github").runFile("Reycko/CCT-Reylib", "src/install/downloader.lua", "master", nil, "silent", "auto", "update")
-]]
-
 fs.makeDir("/startup")
 local f = io.open("/startup/reylib_auto.lua", "w")
 if (f) then
-  f:write(data)
+  f:write([[
+---Reylib auto updater, avoid touching this file.
+
+require(".libs.reylib.github").runFile("Reycko/CCT-Reylib", "src/install/downloader.lua", "master", nil, "silent", "auto", "update")
+]])
   f:flush()
   f:close()
 end
